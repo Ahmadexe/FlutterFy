798db9183f3ef4b67d847b1c0d9e82b3778adc8c
Ahmad <muahmad710@gmail.com>
2022-10-10 00:08:37 +0500

    Adding Channels to home UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fy/utils/colors.dart';
+import 'package:fy/widgets/genre_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey.shade900,
+          foregroundColor: Colors.white,
+          elevation: 10,
+          onPressed: () {},
+          child: const Icon(Icons.add)),
       backgroundColor: mobileBackground,
       appBar: AppBar(
         toolbarHeight: 130,
@@ -22,6 +29,33 @@ class _HomeScreenState extends State<HomeScreen> {
             Text(
               "Ahmad",
               style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Wrap(
+                spacing: 10,
+                children: [
+                  GenreCard(image: "rap.jpg", title: "Rap", onPressed: () {}),
+                  GenreCard(
+                      image: "sadrap.jpg", title: "Sed", onPressed: () {}),
+                  GenreCard(image: "edm.jpg", title: "EDM", onPressed: () {}),
+                  GenreCard(image: "rock.jpg", title: "Rock", onPressed: () {}),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40,),
+            Row(
+              children: const [
+                Text("Famous",style: TextStyle(color: Colors.white, fontSize: 19),),
+                Text(" Channels", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 19),)
+              ],
             )
           ],
         ),
